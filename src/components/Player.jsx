f4a9ec512e2a1c62be9a8d78e78c36c63ad86224
Flavia Kosti <flaviakosti@gmail.com>
2024-03-19 17:53:40 -0400

    React-Refs-Portals: Intial logic

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [player, setPlayer] = useState('');
+  const [submitted, setSubmitted] = useState(false)
+  
+  function handleChange(event){
+    setSubmitted(false);
+    setPlayer(event.target.value);
+  }
+
+  function handleClick(){
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome { (player && submitted) ? player : 'unknown'} entity</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={player} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
