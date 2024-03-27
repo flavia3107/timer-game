b268f0ac37ec4355514e3801ec503cfa66628e77
Flavia Kosti <flaviakosti@gmail.com>
2024-03-27 14:29:50 -0400

    Timer-Game: Show score

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -13,6 +13,8 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
     const dialog = useRef();
     const userLost = remainingTime <= 0;
     const formattedRemaingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000))*100);
+
     useImperativeHandle(ref, () => ({
         open(){
             dialog.current.showModal();
@@ -28,7 +30,9 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
      */
 
     return <dialog ref={dialog} className="result-modal">
-      {userLost && <h2>You lost </h2>}
+        {userLost && <h2>You lost </h2>}
+        {!userLost && <h2>Your Score: {score} </h2>}
+
         <p>The target time was <strong>{targetTime}</strong> seconds. </p>
         <p>You stopped the timer with <strong> {formattedRemaingTime} seconds left</strong> </p>
         <form method="dialog" onSubmit={onReset}>
