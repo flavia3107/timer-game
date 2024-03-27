f134cc23ec7efc10678d67cedfffb072365424a4
Flavia Kosti <flaviakosti@gmail.com>
2024-03-26 21:53:48 -0400

    Timer-Game: Update logic

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -2,28 +2,31 @@ import { useRef, useState } from "react";
 import ResultModal from "./ResultModal";
 
 export default function TimerChallenge({title, targetTime}){
-    const [timerExpired, setTimerExpired] = useState(false);
-    const [timerStarted, setTimerStarted] = useState(false);
-    const dialog = useRef();
-
     /**
-     * Can't use a normal variable since the function get re-exectuted because of useState, 
+     * Can't use a normal variable since the function get re-executed because of useState, 
      * so the variable will be re-initialized everytime the function gets executed.
      * This is why is needed useRef
      */
-    const timer = useRef(); 
-
+    const timer = useRef();
+    const dialog = useRef();
+    const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+    const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+    
+    if(timeRemaining <= 0 ){
+        clearInterval(timer.current);
+        setTimeRemaining(targetTime * 1000);
+        dialog.current.open();
+    }
+   
     function handleStart(){
-       timer.current = setTimeout(() => {
-            setTimerExpired(true);
-            dialog.current.open();
-        }, targetTime * 1000);
-        
-        setTimerStarted(true)
+       timer.current = setInterval(() => {
+            setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 10);
+        },  10);
     }
 
     function handleStop(){
-        clearTimeout(timer.current)
+        dialog.current.open();
+        clearInterval(timer.current);
     }
 
     return <>
@@ -36,10 +39,10 @@ export default function TimerChallenge({title, targetTime}){
             <h2>{title}</h2>
             <p className="challenge-time"> {targetTime} second {targetTime > 1 ? 's' : ''} </p>
             <p>
-                <button onClick={ timerStarted ? handleStop : handleStart}>{ timerStarted ? 'Stop':'Start'} Challenge</button>
+                <button onClick={ timerIsActive ? handleStop : handleStart}>{ timerIsActive ? 'Stop':'Start'} Challenge</button>
             </p>
-            <p className={ timerStarted ? 'active': undefined}>
-                { timerStarted ? 'Time is running ...':' Timer inactive '}
+            <p className={ timerIsActive ? 'active': undefined}>
+                { timerIsActive ? 'Time is running ...':' Timer inactive '}
             </p>
         </section>
     </> 
