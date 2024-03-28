c9c24b8dada6bcea540169333a3a6b4edfd85a0a
Flavia Kosti <flaviakosti@gmail.com>
2024-03-27 21:25:52 -0400

    Timer-Game: Implement portals

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import Player from './components/Player.jsx';
 import TimerChallenge from './components/TimerChallenge.jsx';
 function App() {
   return (
     <>
-      <Player />
+      {/* <Player /> */}
       <div id="challenges">
         <TimerChallenge title="Easy" targetTime={1} />
         <TimerChallenge title="Not easy" targetTime={5} />
--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,5 +1,5 @@
 import {forwardRef, useImperativeHandle, useRef} from 'react';
-
+import { createPortal } from 'react-dom';
 /**
  * forwardRef will forward the ref value from TimeChallenge (parent) to ResultModal (child).
  * To use forwardRef the component function will be wrapped with it, which will return a new component
@@ -29,16 +29,18 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime,
      * Also the hook will always work with the forwardRef hook.
      */
 
-    return <dialog ref={dialog} className="result-modal">
-        {userLost && <h2>You lost </h2>}
-        {!userLost && <h2>Your Score: {score} </h2>}
+    return createPortal(<dialog ref={dialog} className="result-modal" onClose={onReset}>
+            {userLost && <h2>You lost </h2>}
+            {!userLost && <h2>Your Score: {score} </h2>}
 
-        <p>The target time was <strong>{targetTime}</strong> seconds. </p>
-        <p>You stopped the timer with <strong> {formattedRemaingTime} seconds left</strong> </p>
-        <form method="dialog" onSubmit={onReset}>
-            <button>Close</button>
-        </form>
-    </dialog>;
+            <p>The target time was <strong>{targetTime}</strong> seconds. </p>
+            <p>You stopped the timer with <strong> {formattedRemaingTime} seconds left</strong> </p>
+            <form method="dialog" onSubmit={onReset}>
+                <button>Close</button>
+            </form>
+        </dialog>, 
+        document.getElementById('modal')
+    );
 });
 
 export default ResultModal;
